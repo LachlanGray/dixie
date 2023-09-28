8bbbecd1f38fcca2a5c51d1cd29d3095d418fac7
Lachlan <lachlan.gray2000@gmail.com>
2023-09-28 16:10:47 -0400

    dixie can talk

--- dixie.py
+++ dixie.py
@@ -1,7 +1,10 @@
-
 import lmql
 import asyncio
 
+from dataclasses import dataclass
+from lmql.runtime.program_state import ProgramState
+
+# colors {{{
 black = lambda text: f"\033[30m{text}\033[0m"
 red = lambda text: f"\033[31m{text}\033[0m"
 green = lambda text: f"\033[32m{text}\033[0m"
@@ -10,169 +13,104 @@ blue = lambda text: f"\033[34m{text}\033[0m"
 magenta = lambda text: f"\033[35m{text}\033[0m"
 cyan = lambda text: f"\033[36m{text}\033[0m"
 white = lambda text: f"\033[37m{text}\033[0m"
+# }}}
 
-lprompt = green("> ")
-lprompt = "    " + lprompt
+lmql.set_default_model("chatgpt")
 
-class CommandChunk:
-    def __init__(self, command):
-        self.command = command
+@lmql.decorators.streaming
+def to_user(value: str, context: ProgramState):
+    diff = context.get_diff("COMPLETION")
+    if value:
+        parts = value.split("```")
 
-    def __str__(self):
-        return self.command
-
-class ChatBuffer:
-    '''
-    Responsible for storing the chat history and interfacing with the frontend.
-
-    When a completion is made, the tokens are streamed to a buffer here. When the flush
-    function is called, something is done with them, like printing. The entire completion
-    itself is returned from the assistant() function. The assistant function uses the 
-    buffer output writer to communicate with the buffer instance.
-    '''
-    def __init__(self):
-        self.sysem_messages = []
-        self.messages = []
-        self.assistant_buffer = []
-        self.user_buffer = []
-        self.streaming=False
-
-    def stream(self, role: str, chunk: str):
-        if role == "assistant":
-            self.assistant_buffer.append(chunk)
-        else:
-            self.user_buffer.append(chunk)
-
-    def send(self, role: str, message: str): # TODO: replace=True/false
-        self.messages.append((role, message))
-
-    def flush(self):
-        '''
-        Process chunks that have accumulated in the stream buffer. Override for different frontends.
-        '''
-        do_continue = True
-
-        for chunk in self.assistant_buffer:
-            if isinstance(chunk, CommandChunk):
-                if chunk.command == "exit":
-                    do_continue = False
-                else:
-                    raise Exception("Unknown command: " + chunk.command)
-                continue
-
-            print(chunk, end="")
-
-        self.assistant_buffer.clear()
-        return do_continue
-
-    @property
-    def top_chunk(self):
-        return self.assistant_buffer[-1] if self.assistant_buffer else ""
-
-    def __iter__(self):
-        return iter(self.messages)
-
-
-chat_buffer = ChatBuffer()
-
-class BufferOutputWriter:
-    def __init__(self, variable):
-        self.variable = variable
-        self.last_value = None
-        self.buffer = chat_buffer
-
-    async def add_interpreter_head_state(self, variable, head, prompt, where, trace, is_valid, is_final, mask, num_tokens, program_variables):
-        if self.variable is not None:
-
-            value = program_variables.variable_values.get(self.variable, "").strip()
-            prompt_state = program_variables.runtime.root_state.query_head
-
-            reached_end = prompt_state.result is not None
-
-            if self.last_value is None:
-                self.last_value = value
-                self.buffer.stream("assistant", value)
-            else:
-                self.buffer.stream("assistant", value[len(self.last_value):])
-                self.last_value = value
-
+        if "`" in diff:
+            print(green(diff), end='')
             return
 
-    def terminate(self):
-        self.buffer.stream("assistant", CommandChunk("exit"))
+        if len(parts) % 2:
+            print(diff, end='')
+        else:
+            print(green(diff), end='')
 
-writer = BufferOutputWriter("response")
+@lmql.query
+async def name_convo():
+    """lmql
+    message = chat['messages'][0][1]
+    "{:user} In a few words, what's the topic of the following message:\n***\n"
+    "{message}\n***"
+    "{:assistant} The topic is \"[TOPIC] where STOPS_BEFORE(TOPIC, '\"')"
+    TOPIC = TOPIC.split('"')[0] # stops before bug in lmql
+    chat["title"] = TOPIC.strip()
+    """
 
-@lmql.query(model="chatgpt", output_writer=writer)
-async def mouthpiece(buffer):
-    '''lmql
-    for role, content in chat_buffer.messages:
+@lmql.query
+async def reply():
+    """lmql
+    messages = chat['messages']
+    for role, content in messages:
         if role == "assistant":
             "{:assistant}{content}"
         else:
             "{:user}{content}"
+    "{:assistant}[@to_user COMPLETION]"
+    chat["messages"].append(("assistant", COMPLETION))
+    """
 
-    "{:assistant} [response]"
-    buffer.terminate()
-    '''
+async def dispatch(coroutines):
+    tasks = [asyncio.create_task(coro) for coro in coroutines]
+    while tasks:
+        done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        for task in done:
+            try:
+                result = task.result()
+            except Exception as e:
+                raise e # raise the exception for now
 
-async def flush_loop():
-    do_continue = True
-    while do_continue:
 
-        do_continue = chat_buffer.flush()
-        await asyncio.sleep(0.1)
+chat = {
+    "title": "",
+    "messages": []
+}
 
 
-async def dialogue_chat():
+async def main():
     user = ""
-    while user != "exit":
+    while True:
         user = input(blue("> "))
-        chat_buffer.send("user", user)
+        if user == "exit":
+            break
+        chat["messages"].append(("user", user))
+        print()
+        response = await dispatch([reply(), name_convo()])
         print()
 
-        result, _ = await asyncio.gather(mouthpiece(writer), flush_loop())
-        response = result[0].variables["response"]
-        chat_buffer.send("assistant", response)
-        print()
-
-# def provide_commands(question):# {{{
-#     @lmql.query(model="openai/gpt-3.5-turbo", decoder="sample", temperature=0.75)
-#     def get_commands(question):
-#         '''lmql
-#         "{:user}{question}\n"
-#         "{:assistant} Enter the following in your shell:\n"
-#         "```\n"
-#         "[commands]" where STOPS_BEFORE(commands, "```")
-#         return commands.strip()
-#         '''
-
-#     commands = get_commands(question)[0]
-#     print()
-#     print(lprompt + commands.replace("\n", "\n" + lprompt))
-#     print()
-#     print("run? (Y/n)")
-#     yn = input(blue("> "))
-#     while yn not in ["Y", "n"]:
-#         print("Y or n please")
-#         yn = input(blue("> "))
-
-#     if yn == "n":
-#         return
-
-#     for command in commands.split("\n"):
-#         os.system(command)
-
-
-# TODO: make dixie interactive so you can have feedback loops
-# TODO: wrap terminal commands in a 'feedkeys api'; this will enable dixie to interact with other things. The protocol:
-# - includes things like functions and context it can use in backend
-# - method to stream text for the user to frontend
-# - I also want to be able to interupt it (instead of regenrate button, just send another message)
-
-# TODO: fun idea dixie comes with terminal frontend, and knows how build frontends for other programs}}}
-
+    print(chat)
 
 if __name__ == "__main__":
-    asyncio.run(dialogue_chat())
+    asyncio.run(main())
 
+
+# @lmql.query
+# def decide_action(message, actions):
+#     """lmql
+#     "{:system} The user will give you a message to interpret, and you must tell them the best followup action."
+#     "{:user} Here is the message:\n\n"
+#     "{message}"
+#     "And here are the available actions:\n\n"
+#     "{',\n'.join(actions)}"
+#     "{:assistant} The appropriate action is '[ACTION]" where STOPS_BEFORE(ACTION, "'")
+#     return ACTION
+#     """
+
+# @dataclass
+# class Action:
+#     description: str
+#     fn: callable
+
+# actions = {
+#     "reply": Action("Immediately say something to the user.", reply),
+# }
+
+# async def reply(messages):
+#     user_text = messages[-1][1]
+#     action = await decide_action(user_text, list(actions.keys()))
